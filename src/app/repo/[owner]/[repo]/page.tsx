e5f4d26dc8ae800aa2f5662b69cc70612b2efe77
Mohmmaed K Al-Sudani <40903597+abalahb1@users.noreply.github.com>
2025-08-11 23:39:45 +0300

    Update page.tsx

--- src/app/repo/[owner]/[repo]/page.tsx
+++ src/app/repo/[owner]/[repo]/page.tsx
@@ -5,11 +5,14 @@ import { Readme } from "./_components/readme";
 import { RepoDetails } from "./_components/repo-details";
 import { RepoHeader } from "./_components/repo-header";
 
-export default async function RepoPage({
-  params,
-}: {
-  params: { owner: string; repo: string };
-}) {
+interface PageProps {
+  params: {
+    owner: string;
+    repo: string;
+  };
+}
+
+export default async function RepoPage({ params }: PageProps) {
   const fullName = `${params.owner}/${params.repo}`;
 
   const [repository, readme] = await Promise.all([
